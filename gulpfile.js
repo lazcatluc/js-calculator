f76b170265bfdfd34cfe80e49634e338d1d4deae
Lazar <LCLazar@DBDC.luxoft.com>
2016-08-18 15:45:31 +0300

    add gulp and sonar

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp  = require('gulp'),
+    gutil = require('gulp-util'),
+	clean = require('gulp-clean'),
+	concat = require('gulp-concat'),
+	sonar = require('gulp-sonar'),
+	Server = require('karma').Server;
+
+gulp.task('package', ['test'], function() {
+	gulp.src(['./src/**/*.js'])
+      .pipe(concat('app.js'))
+	  .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('clean', function() {
+	gulp.src('./build', {read: false}).pipe(clean());
+	gulp.src('./.sonar', {read: false}).pipe(clean());
+	gulp.src('./reports', {read: false}).pipe(clean());
+});
+
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true,
+	junitReporter: {
+		outputFile: __dirname + '/reports/junit/TESTS-xunit.xml'
+	},
+		
+	coverageReporter: {
+		type:   'lcov',
+		dir:    'reports',
+		subdir: 'coverage'
+	}
+  }, done).start();
+});
+
+gulp.task('sonar', function () {
+    var options = {
+        sonar: {
+            projectKey: 'sonar:js-calculator:1.0.0',
+            projectName: 'js-calculator',
+            projectVersion: '1.0.0',
+            // comma-delimited string of source directories 
+            sources: 'src',
+            language: 'js',
+            sourceEncoding: 'UTF-8',
+            javascript: {
+                lcov: {
+                    reportPath: 'reports/coverage/lcov.info'
+                }
+            },
+            exec: {
+                // All these properties will be send to the child_process.exec method (see: https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback ) 
+                // Increase the amount of data allowed on stdout or stderr (if this value is exceeded then the child process is killed, and the gulp-sonar will fail). 
+                maxBuffer : 1024*1024
+            }
+        }
+    };
+ 
+    // gulp source doesn't matter, all files are referenced in options object above 
+    return gulp.src('thisFileDoesNotExist.js', { read: false })
+        .pipe(sonar(options))
+        .on('error', util.log);
+});
+
+gulp.task('default', ['clean', 'package'], function() {});
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -4,4 +4,15 @@ describe('Calculator', function () {
     expect(add(4,6)).toEqual(10);
   });
   
+  it('should subtract 4 from 6 to a total of 2', function () {
+    expect(subtract(6,4)).toEqual(2);
+  });
+  
+  it('should multiply 4 and 6 to a total of 24', function () {
+    expect(multiply(6,4)).toEqual(24);
+  });
+  
+  it('should divide 6 by 3 to a total of 2', function () {
+    expect(divide(6,3)).toEqual(2);
+  });
 });
